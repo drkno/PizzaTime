61701c9b22fedf8ff895559d115ac7db688ca803
Matthew Knox <matthew@makereti.co.nz>
2017-09-11 14:06:35 +1200

    Improved version

--- public/controller.js
+++ public/controller.js
@@ -3,60 +3,39 @@ pizzaApp.factory('pSocket', function (socketFactory) {
     return socketFactory();
 });
 pizzaApp.controller("PizzaCtrl", function($scope, $interval, pSocket) {
-    let countdownUntil = 0;
-    let prices = {};
-
-    $scope.remainingTime = 362439000;
-    $scope.orders = [];
-    $scope.hasOrdered = false;
-
-    pSocket.on('order', data => {
-        $scope.orders.push(data);
-    });
-
-    pSocket.on('init', data => {
-        $scope.hasOrdered = false;
-        countdownUntil = data.countdownUntil;
-        $scope.orders = data.orders;
-    });
-
     $interval(() => {
-        if (countdownUntil > 0) {
-            $scope.remainingTime = Math.max(countdownUntil - Date.now(), 0);
-        }
+        // if (countdownUntil > 0) {
+        //     $scope.remainingTime = Math.max(countdownUntil - Date.now(), 0);
+        // }
     }, 1000);
 
-    $scope.calcEntitlements = order => {
-        return '';
+    $scope.sendUpdate = user => {
+
     };
 
-    $scope.calcOwes = order => {
-        return 0;
+    $scope.joinOrder = user => {
+        user.joined = true;
+        pSocket.emit('join', user);
     };
 
-    $scope.countPizza = () => {
-        return ['test'];
+    $scope.leaveOrder = user => {
+        user.joined = false;
+        pSocket.emit('leave', user);
     };
 
-    $scope.countMisc = () => {
-        return ['pizza'];
+    $scope.addUser = user => {
+        pSocket.emit('newUser', $scope.newPerson);
     };
 
-    $scope.resetAll = () => {
-        pSocket.emit('reset');
+    $scope.addGarlicKnots = user => {
+
     };
 
-    $scope.placeOrder = () => {
-        const order = {
-            name: document.getElementById('inputName').value,
-            email: document.getElementById('inputEmail').value,
-            flav: document.querySelectorAll('input[name=toppings]:checked').map(e => e.value),
-            add: document.querySelectorAll('input[name=other]:checked').map(e => e.value)
-        };
-        $scope.orders.push(order);
-        pSocket.emit('order', order);
-        $scope.hasOrdered = true;
+    $scope.newPerson = {
+        name: ''
     };
 
-    pSocket.emit('init');
+user.garlicKnots
+user.joined
+removeUser(user)
 });
